f7ce2354078360af4809c3df5db71f7a753638a4
Cristian Arce <cristiancamilo_1989@hotmail.com>
2023-02-06 17:37:25 -0500

    redireccionamiento de rutas a la pagina ppal despues del formulario

--- src/componentes/clientes/NuevoCliente.js
+++ src/componentes/clientes/NuevoCliente.js
@@ -1,9 +1,16 @@
-import React, {Fragment, useState} from 'react'
+import React, { Fragment, useState } from 'react';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import clienteAxios from '../../config/axios';
 
-const NuevoCliente = () => {
+
+
+const NuevoCliente = ({history}) => {
+
+    let navigate = useNavigate();
 
     // cliente = state, guardarCliente = funcion para guardar el state
-    const[cliente, guardarCliente] = useState({
+    const [cliente, guardarCliente] = useState({
         nombre: '',
         apellido: '',
         empresa: '',
@@ -17,15 +24,58 @@ const NuevoCliente = () => {
         guardarCliente({
             // obtener copia del state actual
             ...cliente,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
+    }
+
+    // Añade en la Rest API un cliente nuevo
+    const agregarCliente = e => {
+        e.preventDefault();
+
+        //enviar peticion a axios
+        clienteAxios.post('/clientes', cliente)
+            .then(res => {
+                //validar si hay errores de mongo
+                if (res.data.code === 11000) {
+                    //alerta
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Hubo un error',
+                        text: 'Ese cliente ya esta registrado'
+                })
+
+                } else {
+
+                    //alerta de sweetalert2
+                    Swal.fire(
+                        'Felicidades! registro exitoso',
+                        res.data.mensaje,
+                        'success'
+                    )
+                }
+                
+                // Redireccionar
+                navigate('/', {replace:true});
+            });
+    }
+
+    //validar el formulario
+    const validarCliente = () => {
+        //destructuring
+        const { nombre, apellido, email, empresa, telefono } = cliente;
+
+        //revisar que las propiedades del state tenga contenido, q todos los campos esten llenos para q se habilite el boton
+        let valido = !nombre.length || !apellido.length || !email.length || !empresa.length || !telefono.length;
+
+        //return true o false
+        return valido;
     }
 
     return (
         <Fragment>
             <h2>Nuevo cliente</h2>
 
-            <form>
+            <form onSubmit={agregarCliente}>
                 <legend>Llena todos los campos</legend>
 
                 <div className="campo">
@@ -37,7 +87,7 @@ const NuevoCliente = () => {
                     <label>Apellido:</label>
                     <input type="text" placeholder="Apellido Cliente" name="apellido" onChange={actualizarState} />
                 </div>
-            
+
                 <div className="campo">
                     <label>Empresa:</label>
                     <input type="text" placeholder="Empresa Cliente" name="empresa" onChange={actualizarState} />
@@ -50,11 +100,11 @@ const NuevoCliente = () => {
 
                 <div className="campo">
                     <label>Teléfono:</label>
-                    <input type="email" placeholder="Teléfono Cliente" name="telefono" onChange={actualizarState} />
+                    <input type="tel" placeholder="Teléfono Cliente" name="telefono" onChange={actualizarState} />
                 </div>
 
                 <div className="enviar">
-                        <input type="submit" className="btn btn-azul" value="Agregar Cliente" onChange={actualizarState} />
+                    <input type="submit" className="btn btn-azul" value="Agregar Cliente" disabled={validarCliente()} />
                 </div>
 
             </form>
