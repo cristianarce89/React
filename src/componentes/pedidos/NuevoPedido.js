4495e2c6f81bb7757d9bc98626a93f218ac26a81
Cristian Arce <cristiancamilo_1989@hotmail.com>
2023-02-13 06:05:21 -0500

    consultando la api para obtener los productos, almacenando la busqueda del state, listando los productos, agregando cantidad de los productos, almacenando la cantidad de productos al state

--- src/componentes/pedidos/NuevoPedido.js
+++ src/componentes/pedidos/NuevoPedido.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import clienteAxios from '../../config/axios';
 
 import FormBuscarProducto from './FormBuscarProducto';
+import FormCantidadProducto from './FormCantidadProducto';
+
 
 
 //ERROR DE PROPS
@@ -21,6 +24,9 @@ function NuevoPedido() {
 
     //state
     const [cliente, guardarCliente] = useState({});
+    const [busqueda, guardarBusqueda] = useState('');
+    const [productos, guardarProductos] = useState([]);
+
 
     useEffect(() => {
         // obtener el cliente
@@ -35,12 +41,64 @@ function NuevoPedido() {
 
     }, []);
 
-    const buscarProducto = () =>{
+    const buscarProducto = async e => {
+        e.preventDefault();
+
+        //obtener los productos de la busqueda
+        const resultadoBusqueda = await clienteAxios.post(`/productos/busqueda/${busqueda}`);
+        
+        // si no hay resultados una alaerta, de lo contrario agregarlo al state
+
+        if(resultadoBusqueda.data[0]){
+            
+            let productoResultado = resultadoBusqueda.data[0];
+            // agregar la llave "producto" (copia de id)
+            productoResultado.producto = resultadoBusqueda.data[0]._id;
+            productoResultado.cantidad = 0;
+
+            // ponerlo en el state
+            guardarProductos([...productos, productoResultado])
+
+        }else {
+            //no hay resultados
+            Swal.fire({
+                icon: "error",
+                title: 'No hay resultados',
+                text: 'Realiza otro tipo de busqueda'
+            })
+        }
 
     }
 
-    const leerDatosBusqueda = () => {
+    //Almacenar una busqueda en el state
+    const leerDatosBusqueda = e => {
+        guardarBusqueda( e.target.value );
+    }
 
+    //actualizar la cantidad de productos
+    const restarProductos = i => {
+        // copiar el arreglo original de productos
+        const todosProductos = [...productos];
+
+        //validar si esta en 0 no puede ir mas alla
+        if(todosProductos[i].cantidad === 0) return;
+
+        //decremento
+        todosProductos[i].cantidad--;
+
+        //almacenarlo en el state
+        guardarProductos(todosProductos);
+    }
+
+    const aumentarProductos = i => {
+        // copiar el arreglo para no mutar el original
+        const todosProductos = [...productos];
+
+        // incrementarlo
+        todosProductos[i].cantidad++;
+
+        //almacenarlo en el state
+        guardarProductos(todosProductos);
     }
 
     return (
@@ -59,58 +117,18 @@ function NuevoPedido() {
             />
 
             <ul className="resumen">
-                <li>
-                    <div className="texto-producto">
-                        <p className="nombre">Macbook Pro</p>
-                        <p className="precio">$250</p>
-                    </div>
-                    <div className="acciones">
-                        <div className="contenedor-cantidad">
-                            <i className="fas fa-minus"></i>
-                            <input type="text" name="cantidad" />
-                            <i className="fas fa-plus"></i>
-                        </div>
-                        <button type="button" className="btn btn-rojo">
-                            <i className="fas fa-minus-circle"></i>
-                            Eliminar Producto
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <div className="texto-producto">
-                        <p className="nombre">Macbook Pro</p>
-                        <p className="precio">$250</p>
-                    </div>
-                    <div className="acciones">
-                        <div className="contenedor-cantidad">
-                            <i className="fas fa-minus"></i>
-                            <input type="text" name="cantidad" />
-                            <i className="fas fa-plus"></i>
-                        </div>
-                        <button type="button" className="btn btn-rojo">
-                            <i className="fas fa-minus-circle"></i>
-                            Eliminar Producto
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <div className="texto-producto">
-                        <p className="nombre">Macbook Pro</p>
-                        <p className="precio">$250</p>
-                    </div>
-                    <div className="acciones">
-                        <div className="contenedor-cantidad">
-                            <i className="fas fa-minus"></i>
-                            <input type="text" name="cantidad" />
-                            <i className="fas fa-plus"></i>
-                        </div>
-                        <button type="button" className="btn btn-rojo">
-                            <i className="fas fa-minus-circle"></i>
-                            Eliminar Producto
-                        </button>
-                    </div>
-                </li>
+                {productos.map((producto, index) => (
+                    <FormCantidadProducto 
+                        key={producto.producto}
+                        producto={producto}
+                        restarProductos={restarProductos}
+                        aumentarProductos={aumentarProductos}
+                        index={index}
+                    />
+                ))}
             </ul>
+
+
             <div className="campo">
                 <label>Total:</label>
                 <input type="number" name="precio" placeholder="Precio" readonly="readonly" />
